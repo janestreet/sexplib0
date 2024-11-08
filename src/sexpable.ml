c858b0c9b5d12283ba48c94b6d9f070bab0eb733
public-release <as-public-release@janestreet.com>
2024-11-08 22:02:20 +0000

    v0.18~preview.130.04+450

--- src/sexpable.ml
+++ src/sexpable.ml
@@ -1,26 +1,44 @@
-module type S = sig
-  type t
+module type S_any = sig
+  type t : any
 
   val t_of_sexp : Sexp.t -> t
   val sexp_of_t : t -> Sexp.t
 end
 
-module type S1 = sig
-  type 'a t
+module type S = sig
+  type t
+
+  include S_any with type t := t
+end
+
+module type S_any1 = sig
+  type 'a t : any
 
   val t_of_sexp : (Sexp.t -> 'a) -> Sexp.t -> 'a t
   val sexp_of_t : ('a -> Sexp.t) -> 'a t -> Sexp.t
 end
 
-module type S2 = sig
-  type ('a, 'b) t
+module type S1 = sig
+  type 'a t
+
+  include S_any1 with type 'a t := 'a t
+end
+
+module type S_any2 = sig
+  type ('a, 'b) t : any
 
   val t_of_sexp : (Sexp.t -> 'a) -> (Sexp.t -> 'b) -> Sexp.t -> ('a, 'b) t
   val sexp_of_t : ('a -> Sexp.t) -> ('b -> Sexp.t) -> ('a, 'b) t -> Sexp.t
 end
 
-module type S3 = sig
-  type ('a, 'b, 'c) t
+module type S2 = sig
+  type ('a, 'b) t
+
+  include S_any2 with type ('a, 'b) t := ('a, 'b) t
+end
+
+module type S_any3 = sig
+  type ('a, 'b, 'c) t : any
 
   val t_of_sexp
     :  (Sexp.t -> 'a)
@@ -35,6 +53,12 @@ module type S3 = sig
     -> ('c -> Sexp.t)
     -> ('a, 'b, 'c) t
     -> Sexp.t
+end
+
+module type S3 = sig
+  type ('a, 'b, 'c) t
+
+  include S_any3 with type ('a, 'b, 'c) t := ('a, 'b, 'c) t
 end
 
 module type S_with_grammar = sig
