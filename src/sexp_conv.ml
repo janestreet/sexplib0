6a9019839a20b4c97b56604c2c97b9f769c18fff
Stefan Muenzel <source@s.muenzel.net>
2025-04-27 11:45:39 +0200

    Add floatarray conversion
    Signed-off-by: Stefan Muenzel <source@s.muenzel.net>

--- src/sexp_conv.ml
+++ src/sexp_conv.ml
@@ -61,6 +61,14 @@ let sexp_of_array sexp_of__a ar =
   List !lst_ref
 ;;
 
+let sexp_of_floatarray (ar : floatarray) =
+  let lst_ref = ref [] in
+  for i = Float.Array.length ar - 1 downto 0 do
+    lst_ref := sexp_of_float (Float.Array.unsafe_get ar i):: !lst_ref
+  done;
+  List !lst_ref
+;;
+
 let sexp_of_hashtbl sexp_of_key sexp_of_val htbl =
   let coll ~key:k ~data:v acc = List [ sexp_of_key k; sexp_of_val v ] :: acc in
   List (Hashtbl.fold htbl ~init:[] ~f:coll)
@@ -285,6 +293,17 @@ let array_of_sexp a__of_sexp sexp =
     in
     loop 1 t
   | Atom _ -> of_sexp_error "array_of_sexp: list needed" sexp
+;;
+
+let floatarray_of_sexp sexp : floatarray =
+  match (sexp : Sexp.t) with
+  | List [] -> Float.Array.create 0
+  | List list ->
+    let len = List.length list in
+    let arr = Float.Array.create len in
+    List.iteri ~f:(fun i x -> Float.Array.unsafe_set arr i (float_of_sexp x)) list;
+    arr
+  | Atom _ -> of_sexp_error "floatarray_of_sexp: list needed" sexp
 ;;
 
 let hashtbl_of_sexp key_of_sexp val_of_sexp sexp =
